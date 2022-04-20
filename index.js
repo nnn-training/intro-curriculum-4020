0f75a909cce19fa8266e8e73850627d3898dcd53
nononon <kagura.mellow@gmail.com>
2022-04-20 18:05:09 +0900

    計算問題でpromiseについて理解を深めよう

--- index.js
+++ index.js
@@ -20,3 +20,34 @@ function getC() {
 
 // TODO ここに getA, getB, getC で得られる結果をかけあわせた結果 2431 を標準出力するコードを記述する
 // ただし Promise チェイン(then関数の結果に対するthen関数の呼び出し)を一度は用いて実装をすること
+
+getA()
+  .then(a => {
+    return getB().then(b => {
+      return a * b;
+    });
+  })
+  .then(result => {
+    getC().then(c => {
+      console.log(result * c);
+    });
+  });
+
+
+
+
+const promiseA = getA();
+const promiseB = getB();
+const promiseC = getC();
+
+Promise.all([promiseA,
+  promiseB,
+  promiseC]
+).then(dates => {
+  console.log(dates[0] * dates[1] * dates[2]);
+});
+
+Promise.all([getA(), getB(), getC()]).then(results => {
+  console.log(results[0] * results[1] * results[2]);
+});
+
