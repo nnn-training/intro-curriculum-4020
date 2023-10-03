6f51ec90afe34228d52a8fac6ea071b993322365
hiroto takao <biiiy080121@outlook.com>
2023-10-03 14:21:35 +0900

    Revision of Method chaining.

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 'use strict';
 
-function getA() {
-  return new Promise((resolve) => {
-    setTimeout(() => { resolve(11); }, 1000);
-  });
-}
-
-function getB() {
+function getA() {//getA function is Promise.
   return new Promise((resolve) => {
     setTimeout(() => { resolve(13); }, 1000);
   });
 }
-
-function getC() {
+function getB() {//getB function is promise.
+  return new Promise(resolve => {
+    setTimeout(() => { resolve(13); }, 1000);
+  });
+}
+function getC() {//getC function is Promise.
   return new Promise((resolve) => {
     setTimeout(() => { resolve(17); }, 1000);
   });
@@ -20,3 +18,14 @@ function getC() {
 
 // TODO ここに getA, getB, getC で得られる結果をかけあわせた結果 2431 を標準出力するコードを記述する
 // ただし Promise チェイン(then関数の結果に対するthen関数の呼び出し)を一度は用いて実装をすること
+getA()
+.then(a => {
+   return getB().then(b => {
+       return a * b;
+   });
+})
+.then(result => {
+  getC().then(c => {
+    console.log(result * c);
+  })
+})
